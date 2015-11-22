8a94f044ddfede54b47dfde84ceb76854c05f351
Antun Matanović <antun.matanovic@gmail.com>
2015-11-22 01:11:12 +0100

    popravljen konflikt

--- mobilna/www/js/app.js
+++ mobilna/www/js/app.js
@@ -40,46 +40,69 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.login', {
+    url: '/login',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+       'tab-login': {
+        templateUrl: 'templates/tab-login.html',
+        controller: 'LoginCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
+ .state('tab.registracija', {
+    url: '/registracija',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-registracija': {
+        templateUrl: 'templates/tab-registracija.html',
+        controller: 'RegistracijaCtrl'
+      }
+    }
+  })
+
+
+  .state('tab.mapa', {
+    url: '/mapa',
+    views: {
+      'tab-mapa': {
+        templateUrl: 'templates/tab-mapa.html',
+        controller: 'MapaCtrl'
+      }
+    }
+  })
+
+  .state('tab.statistika', {
+    url: '/statistika',
+    views: {
+      'tab-statistika': {
+        templateUrl: 'templates/tab-statistika.html',
+        controller: 'StatistikaCtrl'
+      }
+    }
+  })
+
+  .state('tab.poruke', {
+    url: '/poruke',
+    views: {
+      'tab-poruke': {
+        templateUrl: 'templates/tab-poruke.html',
+        controller: 'PorukeCtrl'
+      }
+    }
+  })
+
+ .state('tab.igra', {
+    url: '/igra',
+    views: {
+      'tab-igra': {
+        templateUrl: 'templates/tab-igra.html',
+        controller: 'IgraCtrl'
       }
     }
   });
 
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/login');
 
 });
--- mobilna/www/js/controllers.js
+++ mobilna/www/js/controllers.js
@@ -1,28 +1,17 @@
 angular.module('starter.controllers', [])
+.controller('LogoutCtrl', function($scope) {})
 
-.controller('DashCtrl', function($scope) {})
+.controller('LoginCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('RegistracijaCtrl', function($scope) {})
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+.controller('MapaCtrl', function($scope) {})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+.controller('StatistikaCtrl', function($scope) {})
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+.controller('PorukeCtrl', function($scope) {})
+
+.controller('IgraCtrl', function($scope) {})
+
+
+;
